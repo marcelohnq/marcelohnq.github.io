0a05df21635bb18e0db756acc402f964564c7e3b
Marcelo Henrique <henrick939@gmail.com>
2024-01-23 11:10:30 -0300

    add: animation projects fade up

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@
         data() {
             return {
                 spinner: true,
+                delay: 0,
                 projects: []
             };
         },
@@ -24,7 +25,7 @@
             <div v-if="spinner" class="spinner-grow" role="status">
                 <span class="visually-hidden">Loading...</span>
             </div>
-            <div v-for="project in projects" class="card">
+            <div v-for="(project, key) in projects" :key="key" :set="delay=key*100" class="card" data-aos="fade-up" :data-aos-delay="delay">
                 <div class="card-body">
                     <a :href="project.html_url" class="card-link"><i class="bi bi-github"></i> Repository</a>
                     <a v-if="project.homepage" :href="project.homepage" class="card-link"><i class="bi bi-eye"></i> Preview</a>
@@ -32,5 +33,14 @@
                     <p class="card-text">{{ project.description }}</p>
                 </div>
             </div>`
-    }).mount('#app')
+    }).mount('#app');
+
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
 })()
