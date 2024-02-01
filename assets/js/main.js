98174649c1113634c16573e0658cecb768a032f8
Marcelo Henrique <henrick939@gmail.com>
2024-02-01 12:43:30 -0300

    add: stack for each project

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@
         data() {
             return {
                 spinner: true,
-                delay: 0,
                 projects: []
             };
         },
@@ -19,18 +18,39 @@
                 const response = await fetch("https://api.github.com/users/marcelohnq/starred");
                 this.projects = (await response.json()).filter(r => r.owner.login === 'marcelohnq');
                 this.spinner = false;
+            },
+            splitDescription(description) {
+                const split = description.split("|");
+                return split[0];
+            },
+            splitStack(description) {
+                const split = description.split("|");
+
+                if (split.length > 1) {
+                    return split[1].split(",");
+                }
+
+                return [];
             }
         },
         template: `
             <div v-if="spinner" class="spinner-grow" role="status">
                 <span class="visually-hidden">Loading...</span>
             </div>
-            <div v-for="(project, key) in projects" :key="key" :set="delay=key*100" class="card" data-aos="fade-up" :data-aos-delay="delay">
+            <div v-for="(project, key) in projects" 
+                 :key="key" 
+                 :set="temp = { delay: key*100, description: splitDescription(project.description), stacks: splitStack(project.description) }"
+                 class="card" 
+                 data-aos="fade-up" 
+                 :data-aos-delay="temp.delay">
                 <div class="card-body">
                     <a :href="project.html_url" class="card-link"><i class="bi bi-github"></i> Repository</a>
                     <a v-if="project.homepage" :href="project.homepage" class="card-link"><i class="bi bi-eye"></i> Preview</a>
                     <h5 class="card-title">{{ project.name }}</h5>
-                    <p class="card-text">{{ project.description }}</p>
+                    <p class="card-text">{{ temp.description }}</p>
+                    <div class="repo-stack">
+                        <p v-for="stack in temp.stacks">{{ stack }}</p>
+                    </div>
                 </div>
             </div>`
     }).mount('#app');
